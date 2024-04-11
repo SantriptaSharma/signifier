e003603df091dfb8e43ebc143250f2d53fc8f1f3
SantriptaSharma <santripta@gmail.com>
2024-04-11 20:28:50 +0530

    setup: meson + raylib build & hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+#include "raylib.h"
 
-int main(int argc, const char *argv[]) {
-	std::cout << "hi" << std::endl;
-	return 0;
+void frame() {
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    DrawText("Hello world", 190, 200, 20, BLACK);
+
+    EndDrawing();
+}
+
+int main(int argc, const char **argv) {
+    InitWindow(800, 450, "Raylib + Meson + C/C++");
+
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()) {
+        frame();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
